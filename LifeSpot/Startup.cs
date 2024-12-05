5565a70349d81aab4d1fe537d57c07bcefd394a0
Денис Голубев <d.golubev@corp.local>
2024-12-05 09:48:07 +0300

    Задание 27.1.4

--- LifeSpot/Startup.cs
+++ LifeSpot/Startup.cs
@@ -27,6 +27,13 @@ namespace LifeSpot
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapGet("/Static/CSS/index.css", async context =>
+                {
+                    // по аналогии со страницей Index настроим на нашем сервере путь до страницы со стилями, чтобы браузер знал, откуда их загружать
+                    var cssPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "CSS", "index.css");
+                    var css = await File.ReadAllTextAsync(cssPath);
+                    await context.Response.WriteAsync(css);
+                });
                 endpoints.MapGet("/", async context =>
                 {
                     var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "index.html");
