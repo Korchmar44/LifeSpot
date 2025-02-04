78921f2153aa647720f7b2fda213c82b924aca6a
Денис Голубев <d.golubev@corp.local>
2025-02-04 15:49:06 +0300

    Задание 29.3.8

--- LifeSpot/wwwroot/js/index.js
+++ LifeSpot/wwwroot/js/index.js
@@ -1,9 +1,8 @@
-﻿function filterContent() {
-    let inputString = document.getElementsByTagName('input')[0].value.toLowerCase();
+﻿function filterContent(userInput) {
     let elements = document.getElementsByClassName('video-container');
     for (let i = 0; i <= elements.length; i++) {
         let videoText = elements[i].querySelector('.video-title').innerText;
-        if (!videoText.toLowerCase().includes(inputString.toLowerCase())) {
+        if (!videoText.toLowerCase().includes(userInput.toLowerCase())) {
             elements[i].style.display = 'none';
         } else {
             elements[i].style.display = 'inline-block'
@@ -16,7 +15,7 @@ function handleSession() {
     let session = new Map();
     session.set("userAgent", window.navigator.userAgent)
 
-    session.set("age", prompt("Пожалуйста, введите ваш возраст"))
+    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
 
     if (session.get("age") >= 18) {
         let startDate = new Date().toLocaleString();
@@ -28,6 +27,11 @@ function handleSession() {
         alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
         window.location.href = "http://www.google.com"
     }
+
+    return session;
+}
+
+let sessionLog = function logSession(session) {
     for (let result of session) {
         console.log(result)
     }
