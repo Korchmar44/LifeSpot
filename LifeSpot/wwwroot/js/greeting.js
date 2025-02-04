d3dd4426adfd933591bd5fd08e7291f3211aeba2
Денис Голубев <d.golubev@corp.local>
2025-02-04 15:02:27 +0300

    Задание 28.6.4

--- LifeSpot/wwwroot/js/greeting.js
+++ LifeSpot/wwwroot/js/greeting.js
@@ -1,9 +1,22 @@
-﻿let age = prompt("Пожалуйста, введите ваш возраст:");
+﻿let session = new Map();
 
-if (age < 18) {
-    alert("Извините, наш сайт предназначен для лиц старше 18 лет.");
-    window.location.replace("https://www.google.com");
-} else {
-    alert("Приветствуем на LifeSpot\n" +
-        new Date().toLocaleString());
+session.set("userAgent", window.navigator.userAgent)
+
+
+session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
+
+if (session.get("age") >= 18) {
+    let startDate = new Date().toLocaleString();
+
+    alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
+    session.set("startDate", startDate)
+}
+else {
+    alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+    window.location.href = "http://www.google.com"
+    a = true + 20 + "name"
+}
+
+for (let result of session) {
+    console.log(result)
 }
