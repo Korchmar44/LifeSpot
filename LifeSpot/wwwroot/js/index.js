83f5e2df42e9fedbef194e625c7a16fc0508c57e
Денис Голубев <d.golubev@corp.local>
2025-02-05 09:47:39 +0300

    Исправил ошибки

--- LifeSpot/wwwroot/js/index.js
+++ LifeSpot/wwwroot/js/index.js
@@ -1,13 +1,14 @@
 ﻿let session = new Map();
 
-function filterContent(inputParseFunction) {
+function filterContent() {
     let elements = document.getElementsByClassName('video-container');
-    for (let i = 0; i <= elements.length; i++) {
+
+    for (let i = 0; i < elements.length; i++) {
         let videoText = elements[i].querySelector('.video-title').innerText;
         if (!videoText.toLowerCase().includes(inputParseFunction().toLowerCase())) {
             elements[i].style.display = 'none';
         } else {
-            elements[i].style.display = 'inline-block'
+            elements[i].style.display = 'inline-block';
         }
     }
 }
