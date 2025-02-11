d28918520401a70f5bdbb9215d5e82cd91a2fca5
Денис Голубев <d.golubev@corp.local>
2025-02-11 20:00:27 +0300

    Задание 30.3.2

--- LifeSpot/wwwroot/js/testing.js
+++ LifeSpot/wwwroot/js/testing.js
@@ -1,2 +1,11 @@
-﻿let elements = document.getElementsByTagName('*');
-alert(`Количество элементов на странице:  ${elements.length}`);
+﻿const saveInput = function () {
+    // Вытащим значение текстового поля (как у нас уже делается при фильтрации)
+    let currentInput = document.getElementsByTagName('input')[0].value.toLowerCase();
+
+    // Покажем окно с прошлым и новым значением
+    alert('Последний ввод: ' + this.lastInput /* равноценно window.lastInput, так как мы работаем в контексте браузера */
+        + '\n' + 'Текущий ввод: ' + currentInput);
+
+    // Сохраним новое значение в контекст
+    this.lastInput = currentInput;
+}
