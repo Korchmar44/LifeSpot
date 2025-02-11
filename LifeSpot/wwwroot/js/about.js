029625a4091e98908a32ca750fe794faf1a07948
Денис Голубев <d.golubev@corp.local>
2025-02-11 19:44:42 +0300

    Задание 30.2.4

--- LifeSpot/wwwroot/js/about.js
+++ LifeSpot/wwwroot/js/about.js
@@ -1,1 +1,35 @@
-﻿console.log("Javascript для страницы 'о проекте' подключен!")
+﻿function getComment() {
+    // Создаем объект обычного комментария
+    let comment = {}
+
+    // Запросим имя
+    comment.author = prompt("Как вас зовут ?")
+    if (comment.author == null) {
+        return
+    }
+
+    // Запросим текст
+    comment.text = prompt("Оставьте отзыв")
+    if (comment.text == null) {
+        return
+    }
+
+    // Сохраним текущее время
+    comment.date = new Date().toLocaleString()
+
+    // Запросим, хочет ли пользователь оставить полноценный отзыв или это будет обычный комментарий
+    let enableLikes = confirm('Разрешить пользователям оценивать ваш отзыв?')
+
+    if (enableLikes) {
+        // Создадим для отзыва новый объект из прототипа - комментария
+        let review = Object.create(comment)
+        // и добавим ему нужное свойство
+        review.rate = 0;
+
+        // Добавляем отзыв с возможностью пользовательских оценок
+        writeReview(review)
+    } else {
+        // Добавим простой комментарий без возможности оценки
+        writeReview(comment)
+    }
+}
